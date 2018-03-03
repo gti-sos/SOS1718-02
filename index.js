3c71c82e200da812f5fed286838815b9594ead4d
sos171803jamm <joseangelmm97@gmail.com>
2018-03-03 21:19:24 +0000

    version 0

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var cool = require("cool-ascii-faces");
+var app = express();
+
+app.get("/hello",(req,res)=>{
+    res.send("Hello World");
+})
+
+app.listen(process.env.PORT);
+
+console.log(cool());
+
+
