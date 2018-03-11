725e26d97fc285104cc199c7e8bca8651956d080
sos171803jamm <joseangelmm97@gmail.com>
2018-03-11 19:17:40 +0000

    heroku

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser");
 
 app.get("/hello",(req,res)=>{
     res.send("Hello World");
 });
 
-app.use("/",express.static("/home/ubuntu/workspace/SOS1718-02"));
+//app.use("/",express.static("/home/ubuntu/workspace/SOS1718-02/public"));
 
 
 app.listen(process.env.PORT);
