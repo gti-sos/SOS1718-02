ca851536f44b1f9358d25a76cb817fae2bb7868c
sos171803jamm <joseangelmm97@gmail.com>
2018-03-11 19:05:25 +0000

    heroku

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.get("/hello",(req,res)=>{
     res.send("Hello World");
-})
+});
 
 app.use("/",express.static("/home/ubuntu/workspace/SOS1718-02/public"));
 
