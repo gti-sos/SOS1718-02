cd23d3ee517d33df57761d6f48dfd23df51cb1e3
Andres Lorenzo Carvia <wirfen@gmail.com>
2018-04-20 23:21:55 +0000

    SS

--- public/files/expenditures/editCtrl.js
+++ public/files/expenditures/editCtrl.js
@@ -2,15 +2,14 @@
 
 angular.module("ExpendituresApp").controller("EditCtrl", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
     console.log("Edit Ctrl initialized!");
-    var BASE_API_PATH = "/api/v1/expenditures/" + $routeParams.country + "/" + $routeParams.year;
-
+    var BASE_API_PATH = "/api/v2/expenditures/" + $routeParams.country + "/" + $routeParams.year;
+    //var app = angular.module('ExpendituresApp', ['bw.paging'])
     $http.get(BASE_API_PATH).then(function(response) {
         $scope.data = response.data;
     });
 
     $scope.updateExpenditure = function() {
         $http.put(BASE_API_PATH, $scope.data).then(function(response) {
-            console.log($httpParamSerializer($scope.data));
             $location.path("/");
         });
     };
--- public/files/expenditures/listCtrl.js
+++ public/files/expenditures/listCtrl.js
@@ -2,7 +2,7 @@
 
 angular.module("ExpendituresApp").controller("ListCtrl", ["$scope", "$http", "$httpParamSerializer", function($scope, $http, $httpParamSerializer) {
     console.log("List Ctrl initialized!");
-    var BASE_API_PATH = "/api/v1/expenditures";
+    var BASE_API_PATH = "/api/v2/expenditures";
 
     $scope.addExpenditure = function() {
         $http.post(BASE_API_PATH, $scope.newExpenditure).then(function(response) {
@@ -27,32 +27,24 @@ angular.module("ExpendituresApp").controller("ListCtrl", ["$scope", "$http", "$h
     };
 
     $scope.getSearch = function() {
+        if (!$scope.newExpenditure.country) {
+            delete $scope.newExpenditure.country;
+        }
+        if (!$scope.newExpenditure.year) {
+            delete $scope.newExpenditure.year;
+        }
+        if (!$scope.newExpenditure.primary) {
+            delete $scope.newExpenditure.primary;
+        }
+        if (!$scope.newExpenditure.secundary) {
+            delete $scope.newExpenditure.secundary;
+        }
+        if (!$scope.newExpenditure.tertiery) {
+            delete $scope.newExpenditure.tertiery;
+        }
         var query = $httpParamSerializer($scope.newExpenditure);
         console.log(query);
         $http.get(BASE_API_PATH + "/?" + query + "&offset=0&limit=0").then(function(response) {
-            //clearExpenditures();
-            console.log("Country: " + $scope.newExpenditure.country);
-            console.log("Year: " + $scope.newExpenditure.year);
-            console.log("Primary: " + $scope.newExpenditure.primary);
-            console.log("Secundary: " + $scope.newExpenditure.secundary);
-            console.log("Tertiery: " + $scope.newExpenditure.tertiery);
-
-            if (query.country == "") {
-                delete query.country;
-            }
-            if (query.year == "") {
-                delete query.year;
-            }
-            if (query.primary == "") {
-                delete query.primary;
-            }
-            if (query.secundary == "") {
-                delete query.secundary;
-            }
-            if (query.tertiery == "") {
-                delete query.tertiery;
-            }
-            console.log(query);
             $scope.expenditures = response.data;
         });
     };
@@ -70,8 +62,26 @@ angular.module("ExpendituresApp").controller("ListCtrl", ["$scope", "$http", "$h
         });
     }
 
-    function clearExpenditures() {
-        $scope.expenditures = "";
-    }
-    //getExpenditures();
+    $scope.getCount = function() {
+        if (!$scope.newExpenditure.country) {
+            delete $scope.newExpenditure.country;
+        }
+        if (!$scope.newExpenditure.year) {
+            delete $scope.newExpenditure.year;
+        }
+        if (!$scope.newExpenditure.primary) {
+            delete $scope.newExpenditure.primary;
+        }
+        if (!$scope.newExpenditure.secundary) {
+            delete $scope.newExpenditure.secundary;
+        }
+        if (!$scope.newExpenditure.tertiery) {
+            delete $scope.newExpenditure.tertiery;
+        }
+        var query = $httpParamSerializer($scope.newExpenditure);
+        $http.get(BASE_API_PATH + "/?" + query + "&offset=0&limit=0").then(function(response) {
+            $scope.count=response.data.length;
+        });
+    };
+    getExpenditures();
 }]);
