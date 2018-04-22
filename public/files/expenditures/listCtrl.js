6881fa1280ea5c7b2d6ce8947b73f67a72ec32ce
Andres Lorenzo Carvia <wirfen@gmail.com>
2018-04-22 19:46:11 +0000

    Casi

--- public/files/expenditures/listCtrl.js
+++ public/files/expenditures/listCtrl.js
@@ -4,7 +4,10 @@ angular.module("ExpendituresApp").controller("ListCtrl", ["$scope", "$http", "$h
     console.log("List Ctrl initialized!");
     var BASE_API = "/api/v2";
     var BASE_API_PATH = "/api/v2/expenditures";
+    var BASE_API_PATH_LIMIT = "/api/v2/expenditures?&limit=10";
     var offsetP = 0;
+    var dataCount = 0;
+
     $scope.addExpenditure = function() {
         $http.post(BASE_API_PATH, $scope.newExpenditure).then(function(response) {
             $scope.status = "Status: " + response.status;
@@ -44,8 +47,10 @@ angular.module("ExpendituresApp").controller("ListCtrl", ["$scope", "$http", "$h
             delete $scope.newExpenditure.tertiery;
         }
         var query = $httpParamSerializer($scope.newExpenditure);
+        query.limit = 10;
+        query.offset = 10;
         console.log(query);
-        $http.get(BASE_API_PATH + "/?" + query + "&offset=0&limit=10").then(function(response) {
+        $http.get(BASE_API_PATH + "/?" + query).then(function(response) {
             $scope.expenditures = response.data;
         }, function errorCallback(response) {
             console.log("Empty");
@@ -61,40 +66,15 @@ angular.module("ExpendituresApp").controller("ListCtrl", ["$scope", "$http", "$h
     };
 
     function getExpenditures() {
-        $http.get(BASE_API_PATH).then(function(response) {
+        $http.get(BASE_API_PATH_LIMIT + "&offset=0").then(function(response) {
             $scope.expenditures = response.data;
+            dataCount = response.data.length();
         }, function errorCallback(response) {
             console.log("Empty");
             $scope.expenditures = [];
             $scope.status = response.data;
         });
     }
-
-    $scope.getCount = function() {
-        if (!$scope.newExpenditure.country) {
-            delete $scope.newExpenditure.country;
-        }
-        if (!$scope.newExpenditure.year) {
-            delete $scope.newExpenditure.year;
-        }
-        if (!$scope.newExpenditure.primary) {
-            delete $scope.newExpenditure.primary;
-        }
-        if (!$scope.newExpenditure.secundary) {
-            delete $scope.newExpenditure.secundary;
-        }
-        if (!$scope.newExpenditure.tertiery) {
-            delete $scope.newExpenditure.tertiery;
-        }
-        var query = $httpParamSerializer($scope.newExpenditure);
-        $http.get(BASE_API_PATH + "/?" + query + "&offset=0&limit=0").then(function(response) {
-            $scope.count = response.data.length;
-        }, function errorCallback(response) {
-            console.log("Empty");
-            $scope.expenditures = [];
-            $scope.status = response.data;
-        });
-    };
 
     $scope.getExpendituresSecured = function() {
         $http.get(BASE_API + "/secure/expenditures", {
@@ -109,11 +89,19 @@ angular.module("ExpendituresApp").controller("ListCtrl", ["$scope", "$http", "$h
         });
     };
 
-    $scope.getPage = function(Number (p)) {
-        var offsetP = offsetP + p;
+    $scope.getPage = function(p) {
+        offsetP = offsetP + p;
         console.log(offsetP);
         if (offsetP < 0) {
-            $http.get(BASE_API_PATH + "/?" + "&offset=0&limit=10").then(function(response) {
+            $http.get(BASE_API_PATH_LIMIT + "&offset=0").then(function(response) {
+                $scope.expenditures = response.data;
+            }, function errorCallback(response) {
+                console.log("Empty");
+                $scope.expenditures = [];
+            });
+        }
+        else if (false) {
+            $http.get(BASE_API_PATH_LIMIT + "&offset=0").then(function(response) {
                 $scope.expenditures = response.data;
             }, function errorCallback(response) {
                 console.log("Empty");
@@ -121,7 +109,7 @@ angular.module("ExpendituresApp").controller("ListCtrl", ["$scope", "$http", "$h
             });
         }
         else {
-            $http.get(BASE_API_PATH + "/?" + "&offset=" + offsetP + "&limit=10").then(function(response) {
+            $http.get(BASE_API_PATH_LIMIT + "&offset=" + offsetP).then(function(response) {
                 $scope.expenditures = response.data;
             }, function errorCallback(response) {
                 console.log("Empty");
@@ -129,6 +117,5 @@ angular.module("ExpendituresApp").controller("ListCtrl", ["$scope", "$http", "$h
             });
         }
     };
-
     getExpenditures();
 }]);
