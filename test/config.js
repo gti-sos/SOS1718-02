1c3057e941e7f20b4c5945580a2863984130a53d
sos171803jamm <joseangelmm97@gmail.com>
2018-05-27 17:08:34 +0000

    a

--- test/config.js
+++ test/config.js
@@ -1,6 +1,6 @@
 exports.config = {
     seleniumAddress: 'http://localhost:8910',
-    specs: ['Main-test.js'],
+    specs: ['T00-Employments.js','Main-test.js'],
     capabilities: {
         'browserName': 'phantomjs'
     }
