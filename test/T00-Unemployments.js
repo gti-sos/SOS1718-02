1a1c1177e8be141bf8e899ee0d0afca07f961e7b
Andres Lorenzo Carvia <wirfen@gmail.com>
2018-05-27 10:32:21 +0000

    Expenditures

---
+++ test/T00-Unemployments.js
@@ -0,0 +1,13 @@
+var newman =require('newman');
+var path =require('path')
+describe('API should work',function(){
+    newman.run({
+        collection:require(path.join(process.cwd(),"test","Unemployments.json")),
+        reporters:"cli"
+    },function(err){
+        if(err)
+            throw err;
+        else
+            console.log("Collection run complete!");
+    });
+});
